d88c647eec38e29b9a2f5383ab03373546b5bea4
Alex-E-Camacho <alex.e.camacho@gmail.com>
2017-07-24 10:29:59 -0400

    Successfully iterate over 5 videos returned from YTSearch, pass them as props to VideoListItem

---
+++ src/components/VideoListItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+  // const video = props.video; not needed when using {video} inside of callback argument
+  
+  return <li>Video</li>;
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/VideoList'
+const API_KEY = 'AIzaSyCkCI9WlWZY0sjABGarlgNicbpgRc-KNlo'
 
-const App = () => {
-  return <div>Hi!</div>
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
